cacf36ef5a26ad0ad4f2dfd1701a73f965c58a63
Sergey Gorbov <sedoy51289@gmail.com>
2023-02-25 20:12:44 +0300

    Add arduino scetch

---
+++ psmd001.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>
+
+// https://www.silabs.com/documents/public/data-sheets/Si7021-A20.pdf
+#define I2C_ADDRESS 0x40
+#define TEMP_REG 0xE3
+#define HUMIDITY_REG 0xE5
+
+void setup() {
+  Wire.begin();
+  Serial.begin(112500);
+}
+
+int get_data(int addr, int reg) {
+  Wire.beginTransmission(addr);
+  Wire.write(reg);
+  Wire.endTransmission();
+
+  Wire.requestFrom(addr, 2);
+  while(!Wire.available());
+  return ((Wire.read() << 8) | Wire.read());
+}
+
+double get_temperature() {
+  return ((175.72 * get_data(I2C_ADDRESS, TEMP_REG)) / 65536) - 46.85;
+}
+
+double get_humidity() {
+  return ((125.0 * get_data(I2C_ADDRESS, HUMIDITY_REG)) / 65536) - 6;
+}
+
+void loop() {
+  Serial.print("Temperature: "); Serial.println(get_temperature());
+  Serial.print("Humidity: "); Serial.println(get_humidity());
+  delay(2000);
+}
